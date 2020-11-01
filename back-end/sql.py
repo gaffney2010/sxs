661c7c8a1d5a6ea2a2ea2ff2ffe09005097e6486
T.J. Gaffney <gaffney.tj@gmail.com>
2020-10-31 21:52:17 -0500

    Start building sqlite3

--- back-end/sql.py
+++ back-end/sql.py
@@ -1,12 +1,13 @@
 import functools
 import logging
 from enum import Enum
+import sqlite3
 from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import MySQLdb
 import pandas as pd
 
-from sql_config import SQL_CONFIG
+from sql_config import DB_VERSION, SQL_CONFIG
 
 INF = 10000  # Must exceed the number of rows for every table.
 NO_TABLES = 10  # For caching
@@ -16,6 +17,8 @@ NO_RETRY = 3
 TEAM_CW_TABLE = "team_cw"
 TEAM_TEXT_COLUMN = "team_text"
 TEAM_ID_COLUMN = "team_id"
+
+LOCAL_DB = f"/home/gaffney/stacks-by-stacks/back-end/data/local_db/{DB_VERSION}.db"
 
 
 @functools.lru_cache(1)
@@ -34,11 +37,12 @@ class SqlConn(object):
 
             # Connect to SQL with SQL_CONFIG settings.
             logging.debug("Initializing new SqlConn.")
-            cls._instance._conn = MySQLdb.connect(**SQL_CONFIG)
+            cls._instance._mysql_conn = MySQLdb.connect(**SQL_CONFIG)
+            cls._instance._sqlite_conn = sqlite3.connect(LOCAL_DB)
 
         return cls._instance
 
-    def _retry_with_reopen(self, f: Callable):
+    def _retry_mysql_with_reopen(self, f: Callable):
         """Retry, opening _conn between."""
         recent_exception = None
         for _ in range(NO_RETRY):
@@ -47,30 +51,48 @@ class SqlConn(object):
             except Exception as e:
                 recent_exception = e
                 logging.debug("Initializing new SqlConn.")
-                self._conn = MySQLdb.connect(**SQL_CONFIG)
+                self._mysql_conn = MySQLdb.connect(**SQL_CONFIG)
 
         raise(recent_exception)
 
     def sql_query(self, query: str) -> List[Tuple]:
-        """Low-level SQL query."""
+        """Low-level SQL query.
+
+        Reads from local (sqlite) database only.
+
+        Args:
+            query: The query to do the read.
+
+        Returns:
+            A list of tuples, which represent the fields in order.  Can use
+                helper functions to join this with the column names.
+        """
         logging.info(f"Query: {query}")
 
-        def query_instructions():
-            cur = self._conn.cursor()
-            cur.execute(query)
-            return cur.fetchall()
-
-        return self._retry_with_reopen(query_instructions)
+        cur = self._sqlite_conn.cursor()
+        cur.execute(query)
+        return cur.fetchall()
 
     def sql_execute(self, command: str) -> None:
-        """Low-level SQL execute."""
+        """Low-level SQL execute.
+
+        This will get run both locally (sqlite) and remotely (mysql).
+
+        Should always be idempotent, but we leave that to callers.
+
+        Args:
+            command: The statement to execute
+        """
         logging.info(f"Execute: {command}")
 
-        def execute_instructions():
-            self._conn.cursor().execute(command)
-            self._conn.commit()
+        self._sqlite_conn.cursor().execute(command)
+        self._sqlite_conn.commit()
 
-        return self._retry_with_reopen(execute_instructions)
+        def execute_instructions():
+            self._mysql_conn.cursor().execute(command)
+            self._mysql_conn.commit()
+
+        self._retry_mysql_with_reopen(execute_instructions)
 
 
 @functools.lru_cache(NO_TABLES)
---
+++ sql/0001_initial_tables.sql
@@ -0,0 +1,69 @@
+create table if not exists game (
+    game_date int,
+    season int,
+    week int,
+    play_status varchar(255),
+    home_team_id int,
+    away_team_id int,
+    mutual_location bool,
+    home_score int,
+    away_score int,
+    primary key (game_date, home_team_id)
+);
+
+create table if not exists team (
+    team_id int,
+    short_name varchar(255),
+    primary key (team_id)
+);
+
+create table if not exists team_cw (
+    team_text varchar(255),
+    team_id int,
+    primary key (team_text),
+    foreign key (team_id) references team(team_id)
+);
+
+CREATE VIEW `games_with_team_names` AS
+select      x.*,
+            home.short_name as home_team_name,
+            away.short_name as away_team_name
+from        game x
+inner join  team home
+on          x.home_team_id=home.team_id
+inner join  team away
+on          x.away_team_id=away.team_id;
+
+insert into team
+values  (1, "Packers"),
+        (2, "Bears"),
+        (3, "Lions"),
+        (4, "Vikings"),
+        (5, "Cowboys"),
+        (6, "Giants"),
+        (7, "Eagles"),
+        (8, "Washington"),
+        (9, "Falcons"),
+        (10, "Panthers"),
+        (11, "Saints"),
+        (12, "Buccaneers"),
+        (13, "Cardinals"),
+        (14, "Rams"),
+        (15, "49ers"),
+        (16, "Seahawks"),
+        (17, "Ravens"),
+        (18, "Bengals"),
+        (19, "Browns"),
+        (20, "Steelers"),
+        (21, "Bills"),
+        (22, "Dolphins"),
+        (23, "Patriots"),
+        (24, "Jets"),
+        (25, "Texans"),
+        (26, "Colts"),
+        (27, "Jaguars"),
+        (28, "Titans"),
+        (29, "Broncos"),
+        (30, "Chiefs"),
+        (31, "Raiders"),
+        (32, "Chargers");
