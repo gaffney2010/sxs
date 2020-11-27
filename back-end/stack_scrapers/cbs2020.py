6ebe92b9b2f49f61c772b2755c1726161032cda9
T.J. Gaffney <gaffney.tj@gmail.com>
2020-11-27 02:47:47 -0600

    Expand on CBS scraper

--- back-end/stack_scrapers/cbs2020.py
+++ back-end/stack_scrapers/cbs2020.py
@@ -4,7 +4,7 @@ URL like https://www.cbssports.com/nfl/picks/experts/against-the-spread/10/
 """
 #########################
 # Logging logic
-SAFE_MODE = True
+SAFE_MODE = False
 
 from local_config import SXS
 LOG_FOLDER = f"{SXS}/back-end/logs/"
@@ -29,7 +29,6 @@ import re
 
 from bs4 import BeautifulSoup
 
-from stack_scrapers.cbs2020_temp import sample_page
 from shared_tools.scraper_tools import *
 from sql import *
 
@@ -46,66 +45,77 @@ class Game(object):
     home_team: str = attr.ib()
 
 
-cbs_page = "https://www.cbssports.com/nfl/picks/experts/against-the-spread/10/"
+def run_scraper(url: str, sample_page: str) -> str:
+    soup = BeautifulSoup(sample_page, features="html.parser")
+    experts = list()
+    for name_div in soup.findAll("div", class_="AuthorHeadshotAndName-name"):
+        experts.append(_clean(name_div.text))
 
-soup = BeautifulSoup(sample_page, features="html.parser")
-experts = list()
-for name_div in soup.findAll("div", class_="AuthorHeadshotAndName-name"):
-    experts.append(_clean(name_div.text))
+    games = list()
+    for link in soup.findAll(href=re.compile(r"_\w+@\w+")):
+        href = link["href"]
+        link_info = re.search(r"NFL_(\d\d\d\d\d\d\d\d)_(\w+)@(\w+)", href)
+        date, away_team, home_team = (
+            int(link_info.group(1)),
+            link_info.group(2),
+            link_info.group(3),
+        )
+        games.append(
+            Game(href=href, date=date, away_team=away_team, home_team=home_team)
+        )
 
-games = list()
-for link in soup.findAll(href=re.compile(r"_\w+@\w+")):
-    href = link["href"]
-    link_info = re.search(r"NFL_(\d\d\d\d\d\d\d\d)_(\w+)@(\w+)", href)
-    date, away_team, home_team = (
-        int(link_info.group(1)),
-        link_info.group(2),
-        link_info.group(3),
-    )
-    games.append(
-        Game(href=href, date=date, away_team=away_team, home_team=home_team)
-    )
+    for i, gi in enumerate(games):
+        blob = sample_page.split(gi.href)[1]
+        if i < len(games) - 1:
+            blob = blob.split(games[i + 1].href)[0]
 
-for i, gi in enumerate(games):
-    blob = sample_page.split(gi.href)[1]
-    if i < len(games) - 1:
-        blob = blob.split(games[i + 1].href)[0]
+        blob_soup = BeautifulSoup(blob, features="html.parser")
+        for expert, p in zip(
+            experts,
+            blob_soup.findAll("p", class_=re.compile("TableExpertPicks-pick")),
+        ):
+            pick_clause = _clean(p.text)
+            predicted_winner_id = get_team_id(pick_clause.split(" ")[0])
 
-    blob_soup = BeautifulSoup(blob, features="html.parser")
-    for expert, p in zip(
-        experts,
-        blob_soup.findAll("p", class_=re.compile("TableExpertPicks-pick")),
-    ):
-        pick_clause = _clean(p.text)
-        predicted_winner_id = get_team_id(pick_clause.split(" "[0]))
+            home_team_id = get_team_id(gi.home_team)
+            away_team_id = get_team_id(gi.away_team)
 
-        home_team_id = get_team_id(gi.home_team)
-        away_team_id = get_team_id(gi.away_team)
+            # TODO: Share this functionality.
+            if pick_clause.find("-") != -1:
+                spread_favorite = predicted_winner_id
+                spread_amt = float(pick_clause.split("-")[-1])
+            elif pick_clause.find("+") != -1:
+                # Set spread_favorite to the non-predicted-winner
+                spread_favorite = (
+                    away_team_id
+                    if predicted_winner_id == home_team_id
+                    else home_team_id
+                )
+                spread_amt = float(pick_clause.split("+")[-1])
+            else:
+                raise Exception(f"Malformed pick_clause: {pick_clause}")
 
-        # TODO: Share this functionality.
-        if pick_clause.find("-") != -1:
-            spread_favorite = predicted_winner_id
-            spread_amt = float(pick_clause.split("-")[-1])
-        elif pick_clause.find("+") != -1:
-            # Set spread_favorite to the non-predicted-winner
-            spread_favorite = (
-                away_team_id
-                if predicted_winner_id == home_team_id
-                else home_team_id
-            )
-            spread_amt = float(pick_clause.split("+")[-1])
+            new_row = {
+                "expert_id": get_expert_id(expert),
+                "affiliate": "CBS",
+                "game_date": gi.date,
+                "home_team_id": home_team_id,
+                "away_team_id": get_team_id(gi.away_team),
+                "predicted_winner_id_with_spread": predicted_winner_id,
+                "spread_favorite": spread_favorite,
+                "spread_amt": spread_amt,
+                "link": url,
+                "exclude": False,
+            }
+            add_row_to_table("stack", new_row, safe_mode=SAFE_MODE)
 
-        new_row = {
-            "expert_id": get_expert_id(expert),
-            "affiliate": "CBS",
-            "fetched_date": date,
-            "game_date": gi.date,
-            "home_team_id": home_team_id,
-            "away_team_id": get_team_id(gi.away_team),
-            "predicted_winner_id_with_spread": predicted_winner_id,
-            "spread_favorite": spread_favorite,
-            "spread_amt": spread_amt,
-            "link": cbs_page,
-            "exclude": False,
-        }
-        add_row_to_table("stack", new_row, safe_mode=SAFE_MODE)
+
+ALL_URLS = ["https://www.cbssports.com/nfl/picks/experts/against-the-spread/10/"]
+
+# TODO(#29): Share this logic too.
+raw_html_cacher = TimedReadWriteCacher(directory=RAW_HTML_DIR, age_days=1)
+for url in ALL_URLS:
+    logging.info(url)
+    with WebDriver() as driver:
+        page_text = read_url_to_string(url, driver, cacher=raw_html_cacher)
+    run_scraper(url, page_text)
