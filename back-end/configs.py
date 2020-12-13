9bcd1700971857b8e428f7e326fed1a67efba7fa
T.J. Gaffney <5215426+gaffney2010@users.noreply.github.com>
2020-12-13 12:07:36 -0600

    Delete configs.py

--- back-end/configs.py
+++
@@ -1,8 +0,0 @@
-SQL_CONFIG = {"host": "45.93.101.1", "port": 3306,
-              "user": "u995391212_gaffney",
-              "passwd": "BootCooperation9720_1",
-              "db": "u995391212_stackql", }
-
-DB_VERSION = "v1"
-
-ODDS_API_KEY = "d7a0c445e6fc02aad782dfc5a6e0294d"
