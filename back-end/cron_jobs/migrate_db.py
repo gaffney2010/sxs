cd7b0ef15f4935998cbdbc1cc72761c1a562e622
T.J. Gaffney <gaffney.tj@gmail.com>
2021-01-14 02:27:31 -0600

    migrate and materialize

--- back-end/cron_jobs/migrate_db.py
+++ back-end/cron_jobs/migrate_db.py
@@ -3,7 +3,9 @@
 0 6 * * *
 """
 
+import materialize_table
 import translate_data
 
 
+materialize_table.mat()
 translate_data.soft_translate()
--- back-end/materialize_table.py
+++ back-end/materialize_table.py
@@ -1,6 +1,6 @@
 ################################################################################
 # Logging logic, must come first
-SAFE_MODE = True
+SAFE_MODE = False
 from tools.logger import configure_logging
 
 configure_logging(SAFE_MODE)
@@ -8,8 +8,9 @@ configure_logging(SAFE_MODE)
 
 from tools import sql
 
-sql.batch_add_rows_to_table("swsws_table",
-                            sql.pull_everything_from_table(
-                                "stacks_with_stacks_with_stacks",
-                                conn=sql.RemoteSqlConn()),
-                            conn=sql.RemoteSqlConn())
+def mat():
+    sql.batch_add_rows_to_table("swsws_table",
+                                sql.pull_everything_from_table(
+                                    "stacks_with_stacks_with_stacks",
+                                    conn=sql.RemoteSqlConn()),
+                                conn=sql.RemoteSqlConn())
